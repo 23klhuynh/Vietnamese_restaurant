93d9554b76cc7be4041819d99ca2745d9fca53cb
Khang Huynh <23klhuynh@gmail.com>
2025-05-24 23:54:07 -0400

    Improving the home page design.

--- client/src/components/fragments/BusinessDetail.tsx
+++ client/src/components/fragments/BusinessDetail.tsx
@@ -1,9 +1,9 @@
-import Photo from "../../assets/picture1.jpg"
-import { IoMdTime } from "react-icons/io";
-import { FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
+import Photo from "../../assets/bg.jpg";
+/* import { IoMdTime } from "react-icons/io";
+import { FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa"; */
 
 const BusinessDetail: React.FC = () => {
-  const hours = [
+  /* const hours = [
     "Monday: 11:00 AM - 9:00 PM",
     "Tuesday: 11:00 AM - 9:00 PM",
     "Wednesday: 11:00 AM - 9:00 PM",
@@ -11,7 +11,7 @@ const BusinessDetail: React.FC = () => {
     "Friday: 11:00 AM - 9:00 PM",
     "Saturday: 11:00 AM - 9:00 PM",
     "Sunday: 11:00 AM - 9:00 PM"
-  ];
+  ]; */
 
   return (
     <div className="business-detail" id="contact">
@@ -19,7 +19,37 @@ const BusinessDetail: React.FC = () => {
         <img src={Photo} alt="Pho dish" className="business-detail__link" />
       </div>
       <div className="business-detail__description">
-        <div className="business-detail__heading business-detail__info border-b-2 border-black px-6 pt-6">
+        <div className="business-detail__main">
+          <h1>
+            The Art of{" "}
+            <span className="bg-gradient-to-r from-orange-300 to-orange-500 bg-clip-text text-transparent">
+              Perfect
+            </span>{" "}
+            <span className="bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent">
+              Pho
+            </span>{" "}
+          </h1>
+          <div className="business-detail__content">
+            <p>
+              At Pho Viet, we believe that creating the perfect bowl of pho is
+              both an art and a sicence. Our chefs have trained for years in
+              Vietnam to perfect the techniques needed to create authentic
+              soul-warming ramen.
+            </p>
+            <p>
+              Our Broths are simmered for 12-18 hours to extract maximum flavor
+              and richness. We make our noodles fresh daily using a traditional
+              recipe, and our toppings are prepared with meticulous attention to
+              details.
+            </p>
+          </div>
+          <div className="business-detail__facts">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ut
+            ratione et ea quo quos hic quae debitis odio expedita assumenda
+            optio cum, omnis libero iste, excepturi velit quis dolore.
+          </div>
+        </div>
+        {/* <div className="business-detail__heading business-detail__info border-b-2 border-black px-6 pt-6">
           <h1>ABOUT</h1>
           <h2>our restaurant</h2>
         </div>
@@ -39,7 +69,7 @@ const BusinessDetail: React.FC = () => {
           </h2>
           <p>5218 Hwy 70 W suite B</p>
           <h3>Morehead City, NC 28557</h3>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- client/src/components/fragments/Intro.tsx
+++ client/src/components/fragments/Intro.tsx
@@ -1,18 +1,34 @@
 import { Link } from "react-router-dom";
-import { FaFacebook } from "react-icons/fa";
+/* import { FaFacebook } from "react-icons/fa"; */
 
 function Intro() {
   return (
     <article className="welcome-section">
       <div className="welcome-section__container">
-        <h1 className="welcome-section__title ">Welcome to Pho Viet</h1>
-        <h3 className="welcome-section__description ">
+        <h1 className="welcome-section__title ">
+          Welcome to{" "}
+          <span className="bg-gradient-to-r from-orange-300 to-orange-500 bg-clip-text text-transparent">
+            Pho
+          </span>{" "}
+          <span className="bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent">Viet</span> 
+        </h1>
+        {/* <h3 className="welcome-section__description ">
           Experience our authentic Vietnamese dishes made with fresh ingredients
+        </h3> */}
+        <h3 className="welcome-section__description ">
+          Your one-stop destination for authentic, steaming bowls of pho
+          perfection. Savor the rich broths, handmade noodles, and carefully
+          craft toppings!
         </h3>
-        <Link className="welcome-section__btn" to="/menu">
-          VIEW MENU
-        </Link>
-        <a
+        <div className="welcome-section__nav">
+          <Link className="welcome-section__btn" to="/menu">
+            View Menu
+          </Link>
+          <Link className="welcome-section__btn special" to="/menu">
+            Order Now
+          </Link>
+        </div>
+        {/* <a
           className="welcome-section__btn"
           href="https://www.facebook.com/profile.php?id=61559370593017"
           target="_blank"
@@ -20,7 +36,7 @@ function Intro() {
           aria-label="Visit our Facebook page"
         >
           <FaFacebook className=" text-blue-600 text-3xl" />
-        </a>
+        </a> */}
       </div>
     </article>
   );
